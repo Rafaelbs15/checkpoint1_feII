e00e6023aa5af95708665213c8da9e0c99876506
Rafaelbs15 <simoesbrafael@gmail.com>
2022-08-28 17:30:21 -0300

    aplicando melhoras app.js

--- app2.js
+++ app2.js
@@ -1,10 +1,16 @@
+
 //selecionando o botão pela id
-var btnEnviar = document.querySelector("#btnEnviar");
+var btnAdd = document.querySelector("#btnAdd");
+var tituloFilme = document.getElementById("titulo")
+var anoFilme = document.getElementById("ano")
+var descricaoFilme = document.getElementById("descricao")
+var linkImagem = document.getElementById("img")
+
 //acionando função do evento ao clicar no botão "Enviar"
-btnEnviar.addEventListener("click", function(event){
+btnAdd.addEventListener("click", event => {
     event.preventDefault();
     // buscando (mapeando) o formulário inteiro 
-    var formFilme = document.querySelector("#formFilme");
+    /*var formFilme = document.querySelector("#formFilme");*/
     //pedindo para imprimir cada campo do formulário
     /*console.log(formFilme.titulo.value);
     console.log(formFilme.ano.value);
@@ -12,14 +18,23 @@ btnEnviar.addEventListener("click", function(event){
     console.log(formFilme.descricao.value);
     console.log(formFilme.link.value);*/
 
-    
-   
+     //criando cards com o valor digitado no HTML
+     function cards(){
+        let criacardTitulo = document.getElementById("titulo").value;
+        document.querySelector("#tituloDigitado").innerHTML = criacardTitulo
+        let criacardAno = document.getElementById("ano").value;
+        document.querySelector("#anoDigitado").innerHTML = criacardAno
+        let criarcardGenero = document.getElementById("genero").value;
+        document.querySelector("#generoDigitado").innerHTML = criarcardGenero
+        let criacardDescricao = document.getElementById("descricao").value;
+        document.querySelector("#descricaoDigitado").innerHTML = criacardDescricao
+        let criarcardImg = document.getElementById("img").value;
+        document.querySelector("#imgDigitado").innerHTML = `
+            <img src= '${linkImagem.value}' >
+            `
+     }
+     cards()
+
 })
 
- //imprimindo o valor digitado no HTML
-    function capturar () {
-    capturando = document.getElementById("titulo").value;
-    document.getElementById("tituloDigitado").innerHTML = capturando;
-    }
 
-    capturar(titulo)
